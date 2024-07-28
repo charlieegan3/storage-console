92a4f77f43b2047506ee4f248373de9ae83c2678
Charlie Egan <charlieegan3@users.noreply.github.com>
2024-07-28 22:31:42 +0100

    pin flake deps

--- flake.nix
+++ flake.nix
@@ -2,13 +2,22 @@
   description = "storage-console";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "4cf7951a91440879f61e05460441762d59adc017";
+    };
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+      rev = "b1d9ab70662946ef0850d488da1c9019f3a9752a";
+    };
     gomod2nix = {
       type = "github";
       owner = "nix-community";
       repo = "gomod2nix";
-      flake = true;
       rev = "31b6d2e40b36456e792cd6cf50d5a8ddd2fa59a1";
     };
   };
@@ -25,15 +34,17 @@
       {
         formatter = pkgs.nixpkgs-fmt;
 
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            go_1_22
-            golangci-lint
-            gomod2nix.packages.${system}.default
-            (mkGoEnv { pwd = ./.; })
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              go_1_22
+              golangci-lint
+              gomod2nix.packages.${system}.default
+              (mkGoEnv { pwd = ./.; })
 
-            minio
-          ];
+              minio
+            ];
+          };
         };
       }
     );
